0df934ab051745e8fd47966038932e0c5e1fe544
zy89 <yzhiyuan0321@gmail.com>
2019-09-18 20:19:14 +1000

    surface final

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace c_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! I am yzy,this is from Alien again");
+            Console.WriteLine("Hello World! I am yzy,this is from surface final");
             //控制台 dotnet run跑程序
         }
     }

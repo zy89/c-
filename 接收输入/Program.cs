cddd5a25e09ac638000ca777fc52f111e9a5fccf
zy89 <yzhiyuan0321@gmail.com>
2019-09-18 21:44:22 +1000

    接收用户输入

---
+++ 接收输入/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace 接收输入
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            string name;
+            Console.Write("plz type ur name: ");
+            name=Console.ReadLine();
+            Console.WriteLine("Hi "+name);
+            */
+
+            string[] arr_name=new string[5];
+            int[] score=new int[5];
+            for(int i=0;i<arr_name.Length;i++){
+                Console.Write("plz type No"+(i+1)+"of Student's name ");
+                arr_name[i]=Console.ReadLine();
+                Console.Write("plz type No"+(i+1)+"of Student's score ");
+                score[i]=int.Parse(Console.ReadLine());
+            }
+            int sum=0;
+            int avg;
+            for(int i=0;i<score.Length;i++){
+                sum+=score[i];
+            }
+            avg=sum/score.Length;
+            Console.WriteLine("total scores are: "+sum+" and avg is: "+avg);
+        }
+    }
+}

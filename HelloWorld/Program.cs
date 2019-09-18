05352fd3b6f78a5553e612997d12917714374dc3
zy89 <yzhiyuan0321@gmail.com>
2019-09-18 20:17:27 +1000

    Alien再次

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace c_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! I am yzy,this is from surface");
+            Console.WriteLine("Hello World! I am yzy,this is from Alien again");
             //控制台 dotnet run跑程序
         }
     }

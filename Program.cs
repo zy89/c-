8dbb5cdbaf9a95107e1acdee7bcab3df8221ce32
zy89 <yzhiyuan0321@gmail.com>
2019-09-18 16:24:26 +1000

    vscode调试

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! I am yzy");
+            //控制台 dotnet run跑程序
+        }
+    }
+}

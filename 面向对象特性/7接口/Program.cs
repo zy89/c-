d4eb465b5ecef98bc500ffde42fdf379dc7b6162
zy89 <yzhiyuan0321@gmail.com>
2019-09-23 19:06:34 +1000

    接口

---
+++ 面向对象特性/7接口/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _7接口
+{
+    /*
+    接口就是指定一组函数成员，而不实现他们的引用类型。
+    接口只能用来被实现
+     */
+     interface ICatchMice{
+         void CatchMice();//默认public。但是不能加任何访问修饰符，加public也算错
+         //再一组
+         //再一组
+     }
+
+     //
+     Cat:ICatchMice{
+         public void CatchMice(){
+             
+         }
+     }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

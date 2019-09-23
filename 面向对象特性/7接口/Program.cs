84e1f4571ad2c6362e9b370e4472f98b62a0918c
zy89 <yzhiyuan0321@gmail.com>
2019-09-23 21:37:14 +1000

    接口

--- 面向对象特性/7接口/Program.cs
+++ 面向对象特性/7接口/Program.cs
@@ -11,18 +11,48 @@ namespace _7接口
          //再一组
          //再一组
      }
-
-     //
-     Cat:ICatchMice{
-         public void CatchMice(){
-             
-         }
+     //不仅仅是猫才会爬树和抓老鼠
+     interface IClimbTree{
+         void ClimbTree();
      }
+
+     //抽象Pet
+     abstract public class Pet{
+        public Pet(string name){
+            _name=name;
+        }
+        protected string _name;
+        public void PrintName(){
+            Console.WriteLine("Pet's name is "+_name);
+        }
+        abstract public void Speak();//定义成虚方法，因为没有动物叫声会是一样的
+    };
+    //来个猫继承。如果有基类又有多个接口同时继承，基类要写最前
+    public class Cat:Pet,ICatchMice,IClimbTree{
+        public Cat(string name):base(name){}//交由基类初始化
+        public override void Speak(){
+            Console.WriteLine(_name+" is speaking "+"meow meow");
+        }
+        //实现接口
+        public void CatchMice(){
+            Console.WriteLine("Catch mice");
+        }
+        public void ClimbTree(){
+            Console.WriteLine("Climb tree");
+        }
+    }
+     
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cat c = new Cat("Tom2");
+            IClimbTree climb = (IClimbTree)c;
+            c.ClimbTree();
+            climb.ClimbTree();
+            ICatchMice catchM = (ICatchMice)c;
+            c.CatchMice();
+            catchM.CatchMice();
         }
     }
 }

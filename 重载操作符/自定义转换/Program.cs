d625a7f047cf40d7f005ff30b959b3623c77e217
zy89 <yzhiyuan0321@gmail.com>
2019-09-26 18:01:39 +1000

    自定义转换

---
+++ 重载操作符/自定义转换/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace 自定义转换
+{
+    public class Pet{
+        public string Name;
+        public void PrintName(){
+            Console.WriteLine("Pet's name is "+Name);
+        }
+        //虚方法
+        virtual public void Speak(){
+            Console.WriteLine(Name+" is speaking");
+        }
+    }
+        public class Dog:Pet{
+        public Dog(string name){
+            Name=name;
+        }
+        public override void Speak(){
+            Console.WriteLine(Name+" is speaking "+"wow wow");
+        }
+        public static implicit operator Cat(Dog dog){
+            return new Cat(dog.Name);
+        }
+    }
+
+    public class Cat:Pet{
+        public Cat(string name){
+            Name=name;
+        }
+        public override void Speak(){
+            Console.WriteLine(Name+" is speaking "+"meow meow");
+        }
+    }
+
+    //静态类
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog= new Dog("Jack");
+            dog.Speak();
+            Cat cat=dog;//隐式转换
+            cat.Speak();//Jack is speaking meow meow.狗转换成猫
+            cat.PrintName();//Pet's name is Jack
+
+        }
+    }
+}

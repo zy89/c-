c062913864a36ed78242b747496f9b1b71506906
zy89 <yzhiyuan0321@gmail.com>
2019-10-09 11:18:00 +1100

    冒泡

---
+++ 算法/排序/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace 排序
+{
+    class Program
+    {
+        //1.冒泡
+        public static int[] BubbleSort(int[] array){
+            int temp;
+            for(int i=0; i<array.Length-1;i++){
+                for(int j=i+1;j<array.Length;j++){
+                    if(array[i] > array[j]){
+                        temp=array[i];
+                        array[i]=array[j];
+                        array[j]=temp;;
+                    }
+                }
+            }
+            return array;
+        }
+        static void Main(string[] args)
+        {   
+            int[] array = new int[]{2,5,1,4,3};
+            BubbleSort(array);
+            //输出数组
+            for(int i =0;i<array.Length;i++){
+                Console.Write(array[i]+" ");
+            }
+        }
+    }
+}

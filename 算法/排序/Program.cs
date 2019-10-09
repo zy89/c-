df1cb018ca6a20aee7f918f24178ae2b78a47685
zy89 <yzhiyuan0321@gmail.com>
2019-10-09 11:38:27 +1100

    插入

--- 算法/排序/Program.cs
+++ 算法/排序/Program.cs
@@ -4,7 +4,7 @@ namespace 排序
 {
     class Program
     {
-        //1.冒泡
+        //1.冒泡 n^2
         public static int[] BubbleSort(int[] array){
             int temp;
             for(int i=0; i<array.Length-1;i++){
@@ -18,10 +18,28 @@ namespace 排序
             }
             return array;
         }
+
+        //2.插入 n^2
+        public static int[] InsertSort(int[] array){
+            for(int i=1;i<array.Length;i++){
+                for(int j=i;j>0;j--){
+                    if(array[j] <array[j-1]) {
+                        int temp = array[j-1];
+                        array[j-1] = array[j]; 
+                        array[j] = temp;  
+                    }
+                }
+            }
+            return array;
+        }
         static void Main(string[] args)
         {   
             int[] array = new int[]{2,5,1,4,3};
+            /* 
             BubbleSort(array);
+            InsertSort(array);
+            */
+            
             //输出数组
             for(int i =0;i<array.Length;i++){
                 Console.Write(array[i]+" ");
